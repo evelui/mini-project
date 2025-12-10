42311746896349a6288b4af4eaf364cc748b7fb1
Runyi927 <runyi.ye@esmt.berlin>
2025-12-10 10:50:36 +0100

    name adding commit

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 print("Welcome to MAAI Workshop")
 print("Please write your name here\n")
 print("Yaonan_Branch")
+<<<<<<< HEAD
 
 print("Runyi")
+=======
+print("Tsung-Han Tsai")
+>>>>>>> 08672cfd6d59e58ce7aeb4d50cda291e2bb619df
